d0c624850834388fa971846fb2fd082d8ef9e9f4
Alejandro Gomez <alejandrogomezmartin90@gmail.com>
2017-04-15 18:21:43 +0200

    change .conf to .json 2

--- paypoomDaemon.py
+++ paypoomDaemon.py
@@ -1,35 +1,34 @@
 import mcrcon
 import sys
 import os
-import ConfigParser
+import json
 
 
 class server_ARK:
     def __init__ (self, hn, pt, pw):
         self.host = hn
-		self.port = pt
-		self.password = pw
+        self.port = pt
+        self.password = pw
 
-	def connect(self):
-		rcon = mcrcon.MCRcon()
+    def connect(self):
+        rcon = mcrcon.MCRcon()
 
-		print("# connecting to "+self.host+":"+self.port+"...")
-		rcon.connect(self.host, self.port)
+        print("# connecting to "+self.host+":"+str(self.port)+"...")
+        rcon.connect(self.host, self.port)
 
-		print("# logging in...")
-		rcon.login(self.password)
+        print("# logging in...")
+        rcon.login(self.password)
 
-		print("# ready")
+        print("# ready")
 
-		try:
-			while True:
-				response = rcon.command(input('> '))
-				if response:
-					print(response)
-
-		except KeyboardInterrupt:
-			print("\n# disconnecting...")
-			rcon.disconnect()
+        try:
+            while True:
+                response = rcon.command(raw_input('> '))
+                if response:
+                    print(response)
+        except KeyboardInterrupt:
+            print("\n# disconnecting...")
+            rcon.disconnect()
 
 	def getHostname(self):
 		return self.host
@@ -41,20 +40,21 @@ class server_ARK:
 		return self.password
 
 if __name__ == '__main__':
-	"""
-    parser = ConfigParser.RawConfigParser()
-    if os.path.isfile(os.path.join('server_ARK.conf')):
-        parser.read(os.path.join('server_ARK.conf'))
-        server_config = parser._sections
+    if os.path.isfile(os.path.join('server_ARK.json')):
+        json_data = open(os.path.join('server_ARK.json'))
+        server_config = json.load(json_data)
+        json_data.close()
+
     else:
-        print("El fichero server_ARK.conf no existe.")
+        print("El fichero server_ARK.json no existe.")
 
-    server1 = server_ARK("127.0.0.1", str(server_config.get('ARK','rcon_port')), str(server_config.get('ARK','ServerAdminPassword')))
+    for lin1 in server_config:
+        print(lin1)
+
+    host = '127.0.0.1'
+    port = server_config['server_ARK']['rcon_port']
+    password = server_config['server_ARK']['ServerAdminPassword']
+
+    server1 = server_ARK(host, port, password)
+    #server1 = server_ARK('127.0.0.1',int('32330'),password)
     server1.connect()
-	"""
-	host = '127.0.0.1'
-	port = int('32330')
-	password = '54321'
-
-	server1 = server_ARK("127.0.0.1", str(server_config.get('ARK','rcon_port')), str(server_config.get('ARK','ServerAdminPassword')))
-	server1.connect()
